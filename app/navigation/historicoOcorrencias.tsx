03a0bb95760341f7146816c49cb73bcac34d336b
RodrigoBraga <rodrigoobraga7@gmail.com>
2025-10-19 21:30:46 -0300

    feat: cria pagina historico de ocorrencias

---
+++ app/navigation/historicoOcorrencias.tsx
@@ -0,0 +1,18 @@
+import { SafeAreaView, StyleSheet } from "react-native";
+
+export default function HistoricoOcorrenciasScreen() {
+
+    return (
+        <SafeAreaView style={styles.container}>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
